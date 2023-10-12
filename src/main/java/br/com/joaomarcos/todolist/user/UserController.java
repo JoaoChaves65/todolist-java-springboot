9b0847c6c46be316753e44075b07a7f0ce636aff
JoaoChaves65 <joaomarcosgol2000@gmail.com>
2023-10-12 16:01:31 -0300

    first commit

---
+++ src/main/java/br/com/joaomarcos/todolist/user/UserController.java
@@ -0,0 +1,35 @@
+package br.com.joaomarcos.todolist.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Modificador
+ * public
+ * private
+ * protected
+ */
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @PostMapping("/")
+    public UserModel create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) {
+            System.out.println("Usuário já existe");
+            return null;
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
+    }
+}
