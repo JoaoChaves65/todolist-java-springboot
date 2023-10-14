3aced07eb96b9793567224e5a54bec2332c2bfaf
JoaoChaves65 <joaomarcosgol2000@gmail.com>
2023-10-14 09:12:04 -0300

    Verificando se data de início é maior que a de término

--- src/main/java/br/com/joaomarcos/todolist/task/TaskController.java
+++ src/main/java/br/com/joaomarcos/todolist/task/TaskController.java
@@ -26,10 +26,16 @@ public class TaskController {
         taskModel.setIdUser((UUID) idUser);
 
         var currentDate = LocalDateTime.now();
-        //14/10/2023 - Atual
-        //13/10/2023 - startAt
-        if(currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de início / data de término deve ser maior do que a data atual");
+        // 14/10/2023 - Atual
+        // 13/10/2023 - startAt
+        if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("A data de início / data de término deve ser maior do que a data atual");
+        }
+
+        if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("A data de início deve ser menor que a de término");
         }
 
         var task = this.taskRepository.save(taskModel);
